2070c80f0b082fb04404b79ceafa961103259b20
Kunal <kunalkumarmaurya749@gmail.com>
2026-02-11 13:34:42 +0530

    fix: correct variable name for liked videos in LikedVideos component

--- src/Pages/LikedVideos.jsx
+++ src/Pages/LikedVideos.jsx
@@ -3,12 +3,12 @@ import { useSelector } from 'react-redux';
 import MainLayout from '../layout/MainLayout';
 
 const LikedVideos = () => {
-  const { LikedVideos } = useSelector((state) => state.video);
+  const { likedVideos } = useSelector((state) => state.video);
   return (
     <MainLayout>
       <div className="pt-3 md:px-5">
         <h2 className="text-3xl font-bold mb-3 px-3 md:px-0">Liked Videos</h2>
-        <Videos videoArray={LikedVideos} />
+        <Videos videoArray={likedVideos} />
       </div>
     </MainLayout>
   );
